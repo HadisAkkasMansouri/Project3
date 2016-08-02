0c2a0683dca7a2655cfee0e938adf362a5557215
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-02 17:05:23 +0430

    hi

--- src/main/java/ir/dotin/business/AddRealCustomerServlet.java
+++ src/main/java/ir/dotin/business/AddRealCustomerServlet.java
@@ -2,7 +2,6 @@ package ir.dotin.business;
 
 import ir.dotin.dataAccess.RealCustomerDao;
 import ir.dotin.entities.RealCustomer;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
