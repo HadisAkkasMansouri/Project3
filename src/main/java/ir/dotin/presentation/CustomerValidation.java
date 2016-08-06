393f4d6ad08cd9c64aafd1e8b30469ecac62eb89
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-06 18:26:32 +0430

    hi

--- src/main/java/ir/dotin/presentation/CustomerValidation.java
+++ src/main/java/ir/dotin/presentation/CustomerValidation.java
@@ -5,8 +5,27 @@ import ir.dotin.exception.NullRequiredFieldException;
 
 public class CustomerValidation {
 
-    public boolean validateRealCustomer(){
-        return true;
+    public static void validateRealCustomer(String name, String familyName, String fatherName, String birthDate, String nationalId) throws NullRequiredFieldException, DateFormatException{
+
+        if(name.isEmpty()){
+            throw new NullRequiredFieldException("وارد نمودن فیلد نام اجباری است");
+        }
+        if(familyName.isEmpty()){
+            throw new NullRequiredFieldException("وارد نمودن نام خانوادگی اجباری است");
+        }
+
+        if(fatherName.isEmpty()){
+            throw new NullRequiredFieldException("وارد نمودن نام پدر اجباری است");
+        }
+
+        if(birthDate.isEmpty()){
+            throw new NullRequiredFieldException("وارد نمودن تاریخ تولد اجباری است");
+        }
+
+        if(nationalId.isEmpty()){
+            throw new NullRequiredFieldException("وارد نمودن کد ملی اجباری است");
+        }
+
     }
 
     public static void validateLegalCustomer(String companeyName, String economicId, String registrationDate) throws NullRequiredFieldException, DateFormatException{
