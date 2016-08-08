6f9bd9c90a6d6c0d6c6e4be389c16699b121da48
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-08 16:42:41 +0430

    hi

--- src/main/java/ir/dotin/presentation/DeleteLegalCustomerServlet.java
+++ src/main/java/ir/dotin/presentation/DeleteLegalCustomerServlet.java
@@ -1,17 +1,36 @@
 package ir.dotin.presentation;
 
+import ir.dotin.dataaccess.LegalCustomerDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class DeleteLegalCustomerServlet extends HttpServlet{
+public class DeleteLegalCustomerServlet extends HttpServlet {
 
-    protected void deleteLegalCustomerRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        String legalCustomerId = request.getParameter("");
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        addLegalCustomerRequest(request, response);
     }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LegalCustomerDAO legalCustomerDAO = new LegalCustomerDAO();
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter result = response.getWriter();
+        String legalCustomerId = request.getParameter("LegalCustomerId");
+        if(legalCustomerDAO.checkLegalCustomerId(legalCustomerId)){
+            legalCustomerDAO.deleteLegalCustomer(legalCustomerId);
+            result.println("<!DOCTYPE html>");
+            result.println("<html><head>");
+            result.println("<content='text/html; charset=UTF-8'>");
+            result.println("<title>generatedLegalCustomer</title></head>");
+            result.println("<body style='background-color:#000000;'>");
+            result.println("<h1 style = \"color:#fff8dc\"'>اطلاعات مشتری حقوقی وارد شده با موفقیت حذف شد</h1>");
+            result.println("</font></body>");
+            result.println("</html>");
+        }
     }
+}
--- src/main/java/ir/dotin/presentation/DeleteRealCustomerServlet.java
+++ src/main/java/ir/dotin/presentation/DeleteRealCustomerServlet.java
@@ -1,6 +1,35 @@
 package ir.dotin.presentation;
 
+import ir.dotin.dataaccess.RealCustomerDAO;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class DeleteRealCustomerServlet extends HttpServlet{
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        addRealCustomerRequest(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RealCustomerDAO realCustomerDAO = new RealCustomerDAO();
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter result = response.getWriter();
+        String realCustomerId = request.getParameter("RealCustomerId");
+        if(realCustomerDAO.checkRealCustomerId(realCustomerId)){
+            realCustomerDAO.deleteRealCustomer(realCustomerId);
+            result.println("<!DOCTYPE html>");
+            result.println("<html><head>");
+            result.println("<content='text/html; charset=UTF-8'>");
+            result.println("<title>generatedRealCustomer</title></head>");
+            result.println("<body style='background-color:#000000;'>");
+            result.println("<h1 style = \"color:#fff8dc\"'>اطلاعات مشتری حقیقی وارد شده با موفقیت حذف شد</h1>");
+            result.println("</font></body>");
+            result.println("</html>");
+        }
+    }
 }
