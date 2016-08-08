fab58a19695cc2f849cbd309d2095966ad9f802e
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-08 17:01:56 +0430

    hi

--- src/main/java/ir/dotin/presentation/UpdateRealCustomerServlet.java
+++ src/main/java/ir/dotin/presentation/UpdateRealCustomerServlet.java
@@ -1,6 +1,18 @@
 package ir.dotin.presentation;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-public class UpdateRealCustomerServlet extends HttpServlet{
+public class UpdateRealCustomerServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        addRealCustomerRequest(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
