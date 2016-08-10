d0fc1a5f9a98fd64075fda80cefb18c7f5acb955
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-10 11:11:16 +0430

    hi

--- src/main/java/ir/dotin/utility/SingleConnection.java
+++ src/main/java/ir/dotin/utility/SingleConnection.java
@@ -3,19 +3,22 @@ package ir.dotin.utility;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class SingleConnection {
 
+    Logger logger = Logger.getLogger("connectionLog");
     private Connection connect = null;
     private static SingleConnection instance = new SingleConnection();
 
     private SingleConnection(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/dotin?useUnicode=true&characterEncoding=UTF-8" , "root" , "root");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/dotin?useUnicode=true&characterEncoding=UTF-8", "root", "root");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }catch (SQLException e) {
+            logger.severe("The connection to DataBase is aborted!");
             e.printStackTrace();
         }
     }
