9b4165281f31ea1f07564a5633f5cd8e871579a1
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-14 10:18:43 +0430

    hi

--- src/main/java/ir/dotin/presentation/DeleteRealCustomerServlet.java
+++ src/main/java/ir/dotin/presentation/DeleteRealCustomerServlet.java
@@ -3,7 +3,6 @@ package ir.dotin.presentation;
 import ir.dotin.business.CustomerValidation;
 import ir.dotin.dataaccess.RealCustomerDAO;
 import ir.dotin.exception.InvalidEntranceException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,13 +15,11 @@ public class DeleteRealCustomerServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         RealCustomerDAO realCustomerDAO = new RealCustomerDAO();
-//        RealCustomer realCustomer = new RealCustomer();
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
         PrintWriter result = response.getWriter();
-        String realCustomerNumber = request.getParameter("RealCustomerId");
-//        String realCustomerId = realCustomer.getCustomerNumber();
+        String realCustomerNumber = request.getParameter("RealCustomerNumber");
 
         try {
             if (CustomerValidation.validateDeleteRealCustomerId(realCustomerNumber)) {
