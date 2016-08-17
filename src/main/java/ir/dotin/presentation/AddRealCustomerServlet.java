6a748dd76cee3b9f475f5be86501de0df4b0e4a7
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-17 14:32:52 +0430

    hi

--- src/main/java/ir/dotin/presentation/AddRealCustomerServlet.java
+++ src/main/java/ir/dotin/presentation/AddRealCustomerServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class AddRealCustomerServlet extends HttpServlet {
 
@@ -19,7 +18,6 @@ public class AddRealCustomerServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
         response.setCharacterEncoding("UTF-8");
-        PrintWriter result = response.getWriter();
         String name = request.getParameter("Name");
         String familyName = request.getParameter("FamilyName");
         String fatherName = request.getParameter("FatherName");
